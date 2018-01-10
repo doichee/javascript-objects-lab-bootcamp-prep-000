6db906eaaaa86ac8cda8d9cc3f31f998efd5c1f1
Ben Deutsch <ben.deutschiii@gmail.com>
2018-01-10 00:11:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value })
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
+  object[key] = value;
+  return object;
+}
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
 }
